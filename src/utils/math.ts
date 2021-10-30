9b8117e12200cedcb98459f9d462ee28d0c95a00
Phips Peter <phips@asana.com>
2021-10-30 13:41:28 -0700

    Add lerp
    Add lerp for interpolation.

--- src/utils/math.ts
+++ src/utils/math.ts
@@ -6,3 +6,13 @@
  */
 export const snap = (value: number): number =>
     Math.sign(value) * Math.ceil(Math.abs(value));
+
+/**
+ * Interpolates between a & b with weight t.
+ * @param a The starting value
+ * @param b The ending value
+ * @param t The weight
+ * @returns The interpolated value.
+ */
+export const lerp = (a: number, b: number, t: number): number =>
+    a * (1 - t) + b * t;
