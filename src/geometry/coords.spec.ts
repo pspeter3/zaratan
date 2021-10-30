dee4ba1aecde69f7838127bd1846a7de5c8299cb
Phips Peter <phips@asana.com>
2021-10-30 16:56:35 -0700

    Add helper methods for Poisson

--- src/geometry/coords.spec.ts
+++ src/geometry/coords.spec.ts
@@ -1,10 +1,22 @@
-import { readCoords, writeCoords } from "./coords";
+import { createCoords, readCoords, sliceCoords, writeCoords } from "./coords";
 import { Vector } from "./vector";
 
 describe("coords", () => {
+    describe("createCoords", () => {
+        it("should create coordinates", () => {
+            expect(createCoords(2)).toHaveLength(4);
+        });
+    });
+
+    describe("sliceCoords", () => {
+        it("should slice coordinates", () => {
+            expect(sliceCoords(createCoords(2), 1)).toHaveLength(2);
+        });
+    });
+
     describe("writeCoords", () => {
         it("should write the values", () => {
-            const coords = new Int32Array(4);
+            const coords = new Int16Array(4);
             writeCoords(coords, 0, new Vector(3, 4));
             writeCoords(coords, 1, new Vector(6, 8));
             expect(coords[0]).toBe(3);
@@ -16,7 +28,7 @@ describe("coords", () => {
 
     describe("readCoords", () => {
         it("should read the values", () => {
-            const coords = Int32Array.from([3, 4, 6, 8]);
+            const coords = Int16Array.from([3, 4, 6, 8]);
             expect(readCoords(coords, 1)).toEqual({ x: 6, y: 8 });
         });
     });
--- src/geometry/coords.ts
+++ src/geometry/coords.ts
@@ -1,15 +1,25 @@
 import { Vector } from "./vector";
 
-/**
- * Coordinates interface.
- */
-export interface Coords {
-    [index: number]: number;
-}
-
 const DIMENSIONS = 2;
 const xOffset = (index: number): number => DIMENSIONS * index;
 const yOffset = (index: number): number => xOffset(index) + 1;
+
+/**
+ * Create coordinates with a capacity.
+ * @param capacity The capacity
+ * @returns The coordinates
+ */
+export const createCoords = (capacity: number): Int16Array =>
+    new Int16Array(DIMENSIONS * capacity);
+
+/**
+ * Slice the coordinates based on the count
+ * @param coords The coordinates to slice
+ * @param count The count
+ * @returns The sliced coordinates
+ */
+export const sliceCoords = (coords: Int16Array, count: number): Int16Array =>
+    coords.slice(0, DIMENSIONS * count);
 
 /**
  * Writes a vector to coordinates.
@@ -18,7 +28,7 @@ const yOffset = (index: number): number => xOffset(index) + 1;
  * @param vector The vector to write
  */
 export const writeCoords = (
-    coords: Coords,
+    coords: Int16Array,
     index: number,
     { x, y }: Vector,
 ): void => {
@@ -32,5 +42,5 @@ export const writeCoords = (
  * @param index The index to read at
  * @returns The vector
  */
-export const readCoords = (coords: Coords, index: number): Vector =>
+export const readCoords = (coords: Int16Array, index: number): Vector =>
     new Vector(coords[xOffset(index)], coords[yOffset(index)]);
