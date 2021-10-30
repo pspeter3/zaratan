1b1aac5ae5d2751e1812999e38dd5f46e520f03e
Phips Peter <pspeter3@users.noreply.github.com>
2021-10-30 13:44:55 -0700

    Create Bounds (#5)
    * Create Bounds
    
    Create the bounds class.
    
    * Add lerp
    
    Add lerp for interpolation.
    
    * Add center
    
    Add the center of bounds.

---
+++ src/geometry/bounds.ts
@@ -0,0 +1,113 @@
+import { ORIGIN } from "../utils/constants";
+import { Vector } from "./vector";
+
+/**
+ * Bounds for a 2D space.
+ */
+export class Bounds {
+    /**
+     * Creates a Bounds from a size.
+     * @param size The size of the bounds
+     * @returns The new bounds
+     */
+    static fromSize(size: Vector): Bounds {
+        return new Bounds(ORIGIN, size);
+    }
+
+    /**
+     * The origin of the bounds.
+     */
+    readonly origin: Vector;
+    /**
+     * The size of the bounds.
+     */
+    readonly size: Vector;
+
+    /**
+     * Constructs a new Bounds
+     * @param origin The origin
+     * @param size The size
+     */
+    constructor(origin: Vector, size: Vector) {
+        this.origin = origin;
+        this.size = size;
+    }
+
+    /**
+     * The x component of the origin.
+     */
+    get x(): number {
+        return this.origin.x;
+    }
+
+    /**
+     * The y component of the origin.
+     */
+    get y(): number {
+        return this.origin.y;
+    }
+
+    /**
+     * The x component of the size.
+     */
+    get width(): number {
+        return this.size.x;
+    }
+
+    /**
+     * The y component of the size.
+     */
+    get height(): number {
+        return this.size.y;
+    }
+
+    /**
+     * The minimum x component.
+     */
+    get xMin(): number {
+        return this.x;
+    }
+
+    /**
+     * The maximum x component.
+     */
+    get xMax(): number {
+        return this.x + this.width;
+    }
+
+    /**
+     * The minimum y component.
+     */
+    get yMin(): number {
+        return this.y;
+    }
+
+    /**
+     * The maximum y component.
+     */
+    get yMax(): number {
+        return this.y + this.height;
+    }
+
+    /**
+     * Checks if the vector is in bound inclusive.
+     * @param vector The vector
+     * @returns Whether the vector is in bounds
+     */
+    contains(vector: Vector): boolean {
+        return (
+            vector.x >= this.xMin &&
+            vector.x <= this.xMax &&
+            vector.y >= this.yMin &&
+            vector.y <= this.yMax
+        );
+    }
+
+    /**
+     * Computes the center of the bounds.
+     * @returns The center
+     */
+    center(): Vector {
+        return this.origin.add(this.size.scale(0.5));
+    }
+}
--- src/utils/math.spec.ts
+++ src/utils/math.spec.ts
@@ -1,4 +1,4 @@
-import { snap } from "./math";
+import { lerp, snap } from "./math";
 
 describe("math", () => {
     describe("snap", () => {
@@ -12,6 +12,20 @@ describe("math", () => {
 
         it("should handle zero", () => {
             expect(snap(0)).toBe(0);
+        });
+    });
+
+    describe("lerp", () => {
+        it("should return a when t is 0", () => {
+            expect(lerp(1.1, 2.1, 0)).toBe(1.1);
+        });
+
+        it("should return b when t is 0", () => {
+            expect(lerp(1.1, 2.1, 1)).toBe(2.1);
+        });
+
+        it("should return the midpoint when t is 0.5", () => {
+            expect(lerp(1.1, 2.1, 0.5)).toBe(1.6);
         });
     });
 });
