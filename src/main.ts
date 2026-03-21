3d6138643948dfec0f8b71b758bf54623edba373
Phips Peter <pspeter3@users.noreply.github.com>
2026-03-21 15:22:40 -0400

    Simplify render pipeline (#5)
    Simplify the render pipeline.

--- src/main.ts
+++ src/main.ts
@@ -2,19 +2,12 @@ import type { OffscreenWorkerMessage } from "./offscreen-worker";
 
 import "./style.css";
 
-const SIZE = 1024;
-
 const canvas = document.getElementById("demo-canvas") as HTMLCanvasElement;
-
-canvas.width = SIZE;
-canvas.height = SIZE;
 
 const worker = new Worker(new URL("./offscreen-worker.ts", import.meta.url), { type: "module" });
 const offscreenCanvas = canvas.transferControlToOffscreen();
 const message: OffscreenWorkerMessage = {
   canvas: offscreenCanvas,
-  width: SIZE,
-  height: SIZE,
 };
 
 worker.postMessage(message, [offscreenCanvas]);
