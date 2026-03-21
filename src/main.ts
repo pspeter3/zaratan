7b1ea82b56b79291e3c098b543a2ab347ce6c943
Phips Peter <phips@asana.com>
2026-03-21 17:44:49 -0400

    Add basic rendering

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,23 @@
-import type { OffscreenWorkerMessage } from "./offscreen-worker";
+import type { OffscreenInitMessage, OffscreenStageMessage } from "./offscreen-worker";
 
 import "./style.css";
 
-const canvas = document.getElementById("demo-canvas") as HTMLCanvasElement;
+const canvas = document.createElement("canvas");
+const list = document.getElementById("list") as HTMLUListElement;
+const itemTmpl = document.getElementById("item") as HTMLTemplateElement;
 
 const worker = new Worker(new URL("./offscreen-worker.ts", import.meta.url), { type: "module" });
 const offscreenCanvas = canvas.transferControlToOffscreen();
-const message: OffscreenWorkerMessage = {
+const message: OffscreenInitMessage = {
   canvas: offscreenCanvas,
 };
 
 worker.postMessage(message, [offscreenCanvas]);
+worker.addEventListener("message", addStage);
+
+function addStage({ data: { name, blob } }: MessageEvent<OffscreenStageMessage>): void {
+  const item = document.importNode(itemTmpl.content, true);
+  item.querySelector("figcaption")!.textContent = name;
+  item.querySelector("img")!.src = URL.createObjectURL(blob);
+  list.appendChild(item);
+}
