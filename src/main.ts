501100044b251cc25acc14a6a07e26325337ffea
Phips Peter <phips@asana.com>
2026-03-22 15:00:46 -0400

    Remove render stages
    Prepare for live editing.

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,8 @@
-import type { OffscreenInitMessage, OffscreenStageMessage } from "./offscreen-worker";
+import type { OffscreenInitMessage } from "./offscreen-worker";
 
 import "./style.css";
 
-const canvas = document.createElement("canvas");
-const list = document.getElementById("list") as HTMLUListElement;
-const itemTmpl = document.getElementById("item") as HTMLTemplateElement;
+const canvas = document.getElementById("canvas") as HTMLCanvasElement;
 
 const worker = new Worker(new URL("./offscreen-worker.ts", import.meta.url), { type: "module" });
 const offscreenCanvas = canvas.transferControlToOffscreen();
@@ -13,18 +11,3 @@ const message: OffscreenInitMessage = {
 };
 
 worker.postMessage(message, [offscreenCanvas]);
-worker.addEventListener("message", addStage);
-
-function addStage({ data: { name, blob } }: MessageEvent<OffscreenStageMessage>): void {
-  const item = document.importNode(itemTmpl.content, true);
-  const caption = item.querySelector("figcaption");
-  const image = item.querySelector("img");
-
-  if (caption === null || image === null) {
-    throw new Error("Item template is missing required elements.");
-  }
-
-  caption.textContent = name;
-  image.src = URL.createObjectURL(blob);
-  list.appendChild(item);
-}
